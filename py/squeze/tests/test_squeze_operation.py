63df597979d1c5c0a9936dd78a8c1afb37281431
Ignasi Pérez Ràfols <iprafols@gmail.com>
2020-04-29 11:31:06 +0200

    added tests for squeze_operation

---
+++ py/squeze/tests/test_squeze_operation.py
@@ -0,0 +1,41 @@
+"""
+    SQUEzE
+    ======
+
+    This file contains tests related to the testing mode of SQUEzE
+"""
+import unittest
+import os
+
+from squeze.tests.abstract_test import AbstractTest
+from squeze.common_functions import deserialize, load_json
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class TestSquezeOperation(AbstractTest):
+    """Test the operation mode
+
+        CLASS: TestSquezeCandidates
+        PURPOSE: Test candidatse mode of squeze
+        """
+    def test_squeze_operation(self):
+        """ Run squeze_operation.py"""
+
+        in_file = "{}/data/formatted_boss_test2.json".format(THIS_DIR)
+        out_file = "{}/results/operation_boss_test2.json".format(THIS_DIR)
+        test_file = "{}/data/candidates_operation_boss_test2_pred.json".format(THIS_DIR)
+        model_file = "{}/data/candidates_boss_test1_nopred_model.json".format(THIS_DIR)
+
+        command = ["squeze_operation.py",
+                   "--model", model_file,
+                   "--output-candidates",
+                   out_file,
+                   "--input-spectra",
+                   in_file,
+                   ]
+        self.run_command(command)
+        self.assertTrue(os.path.isfile(out_file))
+        self.compare_data_frames(test_file, out_file)
+
+if __name__ == '__main__':
+    unittest.main()
