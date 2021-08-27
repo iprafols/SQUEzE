74ba4389b0da1e872aeba8ecd042c9779239cd18
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2021-08-27 15:59:14 +0200

    removed old code

--- py/squeze/random_forest_classifier.py
+++ py/squeze/random_forest_classifier.py
@@ -183,104 +183,6 @@ class RandomForestClassifier(object):
         # discard the sklearn model
         del rf
 
-    def __loadTreeFromForest(self, tree_index):
-        """ Loads one tree from the forest file and checks that
-            the recursion limit is enough
-
-            Parameters
-            ----------
-            tree_index : int
-            Index of the location of the desired tree in self.__trees
-
-            """
-        self.__children_left = self.__trees[tree_index]["children_left"]
-        self.__children_right = self.__trees[tree_index]["children_right"]
-        self.__feature = self.__trees[tree_index]["feature"]
-        self.__threshold = self.__trees[tree_index]["threshold"]
-        self.__tree_proba = self.__trees[tree_index]["proba"]
-
-        if len(self.__children_left) > sys.getrecursionlimit():
-            sys.setrecursionlimit(int(len(self.__children_left)*1.2))
-
-    def __searchNodes(self, indexs, node_id=0):
-        """ Recursively navigates in the tree and calculate the tree response
-            by updating the values stored in self.__proba
-
-            Parameters
-            ----------
-            indexs :
-            Indexs to question
-            """
-        # find left child
-        left_child_id = self.__children_left[node_id]
-
-        # chek if we are on a leave load probabilities and return
-        if left_child_id == -1:
-            for i in indexs:
-                self.__proba[i] = self.__tree_proba[node_id]
-            return
-
-        # find right child
-        right_child_id = self.__children_right[node_id]
-
-        # find split characteristics
-        feature = self.__feature[node_id]
-        threshold = self.__threshold[node_id]
-
-        # split samples
-        left_cond = (self.__X[indexs, feature] <= threshold)
-        left_child_indexs = indexs[left_cond]
-        right_child_indexs = indexs[~left_cond]
-
-        # navigate the tree
-        print(node_id, self.__children_left[node_id], self.__children_right[node_id], len(self.__children_left))
-        self.__searchNodes(left_child_indexs, node_id=left_child_id)
-        self.__searchNodes(right_child_indexs, node_id=right_child_id)
-
-        return
-
-    def predict_proba(self, X):
-        """ Predict class probabilities for X
-
-            Parameters
-            ----------
-            Refer to sklearn.ensemble.RandomForestClassifier.predic_proba
-            """
-
-        output = np.zeros((len(X), self.__num_categories))
-        #self.__X = X.copy()
-        #self.__proba = np.zeros((len(X), self.__num_categories))
-
-        for tree_index in np.arange(self.__num_trees):
-            #self.__loadTreeFromForest(tree_index)
-            #self.__searchNodes(np.arange(len(X)))
-            #output += self.__proba
-            proba = np.zeros((len(X), self.__num_categories))
-            children_left = self.__trees[tree_index]["children_left"]
-            children_right = self.__trees[tree_index]["children_right"]
-            features = self.__trees[tree_index]["feature"]
-            thresholds = self.__trees[tree_index]["threshold"]
-            tree_proba = self.__trees[tree_index]["proba"]
-            indexs = np.arange(X.shape[0], dtype=int)
-            """import numba
-            print("children_left", children_left.dtype, numba.typeof(children_left))
-            print("children_right", children_right.dtype, numba.typeof(children_right))
-            print("features", features.dtype, numba.typeof(features))
-            print("thresholds", thresholds.dtype, numba.typeof(thresholds))
-            print("tree_proba", tree_proba.dtype, numba.typeof(tree_proba))
-            print("indexs", indexs.dtype, numba.typeof(indexs))"""
-            if len(children_left) > sys.getrecursionlimit():
-                sys.setrecursionlimit(int(len(children_left)*1.2))
-            search_nodes(X, children_left, children_right, features, thresholds,
-                         tree_proba, proba, indexs, 0)
-            output += proba
-            #raise Exception("No need to continue")
-
-        output /= self.__num_trees
-
-        #del self.__X
-        return output
-
     @classmethod
     def from_json(cls, data):
         """ This function deserializes a json string to correclty build the class.
@@ -351,8 +253,36 @@ class RandomForestClassifier(object):
                     (hdu.data["proba"].shape[0], 1, hdu.data["proba"].shape[1])),
                 } for hdu in hdus]
         cls_instance.set_trees(trees)
-        
+
         return cls_instance
+
+    def predict_proba(self, X):
+        """ Predict class probabilities for X
+
+            Parameters
+            ----------
+            Refer to sklearn.ensemble.RandomForestClassifier.predic_proba
+            """
+
+        output = np.zeros((len(X), self.__num_categories))
+
+        for tree_index in np.arange(self.__num_trees):
+            proba = np.zeros((len(X), self.__num_categories))
+            children_left = self.__trees[tree_index]["children_left"]
+            children_right = self.__trees[tree_index]["children_right"]
+            features = self.__trees[tree_index]["feature"]
+            thresholds = self.__trees[tree_index]["threshold"]
+            tree_proba = self.__trees[tree_index]["proba"]
+            indexs = np.arange(X.shape[0], dtype=int)
+            if len(children_left) > sys.getrecursionlimit():
+                sys.setrecursionlimit(int(len(children_left)*1.2))
+            search_nodes(X, children_left, children_right, features, thresholds,
+                         tree_proba, proba, indexs, 0)
+            output += proba
+
+        output /= self.__num_trees
+
+        return output
 
     def set_num_trees(self, num_trees):
         """ Set the variable __num_trees. Should only be called from the method from_json"""
