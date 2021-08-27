a3591558066133c0728c807cedb2b3822841eb2e
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2021-08-27 15:57:29 +0200

    search_node function is now numba-optimized

--- py/squeze/random_forest_classifier.py
+++ py/squeze/random_forest_classifier.py
@@ -19,8 +19,93 @@ import sys
 
 import numpy as np
 import astropy.io.fits as fits
+import numba
+from numba import prange, jit
 
 from squeze.common_functions import deserialize
+
+@jit(nopython=True,
+     locals=dict(X=numba.types.float64[:, :],
+                 children_left=numba.types.int64[:],
+                 children_right=numba.types.int64[:],
+                 thresholds=numba.types.float64[:],
+                 tree_proba=numba.types.float64[:, :, :],
+                 proba=numba.types.float64[:, :],
+                 indexs=numba.types.int64[:],
+                 node_id=numba.types.int64),
+     )
+def search_nodes(X, children_left, children_right, features, thresholds,
+                tree_proba, proba, indexs, node_id):
+    """ Recursively navigates in the tree and calculate the tree response
+        by updating the values stored in self.__proba
+
+        Parameters
+        ----------
+        X : array of floats
+        The dataset to classify. Each line contains the information for a given
+        candidate
+
+        children_left : array of int
+        For node i, j=children_left[i] is the position of left tree node. -1
+        indicates a leaf node
+
+        children_right : array of int
+        For node i, j=children_left[i] is the position of right tree node. -1
+        indicates a leaf node
+
+        features : array of int
+        For node i, j=features[i] is the index of the feature used to split the
+        dataset
+
+        thresholds : array of float
+        For node i, y=thresholds[i] is the threshold used to split the
+        dataset. Candidates where its feature j is evaluated to less or equal y
+        are assigned to the left child node, others are assigned to the right
+        child node
+
+        tree_proba : array of float
+        Probabilities for the different leafs of the tree and for the different
+        classes
+
+        proba : array of float
+        Probabilities assigned by the tree to the different candidates
+
+        index : array of int
+        Indexs of the candidates being evaluated
+
+        node_id : int
+        Current node
+        """
+    # find left child
+    left_child_id = children_left[node_id]
+
+    # chek if we are on a leave load probabilities and return
+    if left_child_id == -1:
+        for i in prange(indexs.size):
+            proba[indexs[i]] = tree_proba[node_id]
+        return proba
+
+    # find right child
+    right_child_id = children_right[node_id]
+
+    # find split characteristics
+    feature = features[node_id]
+    threshold = thresholds[node_id]
+
+    # split samples
+    left_cond = (X[indexs, feature] <= threshold)
+    left_child_indexs = indexs[left_cond]
+    right_child_indexs = indexs[~left_cond]
+
+    # navigate the tree
+    proba = search_nodes(X, children_left, children_right, features,
+                thresholds, tree_proba, proba, left_child_indexs,
+                left_child_id)
+    proba = search_nodes(X, children_left, children_right, features,
+                thresholds, tree_proba, proba, right_child_indexs,
+                right_child_id)
+
+    return proba
 
 class RandomForestClassifier(object):
     """ The purpose of this class is to create a RandomForestClassifier
@@ -83,12 +168,12 @@ class RandomForestClassifier(object):
             tree_sklearn = dt.tree_
 
             tree = {}
-            tree["children_left"] = tree_sklearn.children_left
-            tree["children_right"] = tree_sklearn.children_right
-            tree["feature"] = tree_sklearn.feature
-            tree["threshold"] = tree_sklearn.threshold
-            value = tree_sklearn.value
-            proba = tree_sklearn.value
+            tree["children_left"] = tree_sklearn.children_left.astype(int)
+            tree["children_right"] = tree_sklearn.children_right.astype(int)
+            tree["feature"] = tree_sklearn.feature.astype(int)
+            tree["threshold"] = tree_sklearn.threshold.astype(float)
+            value = tree_sklearn.value.astype(float)
+            proba = tree_sklearn.value.astype(float)
             for i, p in enumerate(proba):
                 proba[i] = p/p.sum()
             tree["proba"] = proba
@@ -148,6 +233,7 @@ class RandomForestClassifier(object):
         right_child_indexs = indexs[~left_cond]
 
         # navigate the tree
+        print(node_id, self.__children_left[node_id], self.__children_right[node_id], len(self.__children_left))
         self.__searchNodes(left_child_indexs, node_id=left_child_id)
         self.__searchNodes(right_child_indexs, node_id=right_child_id)
 
@@ -162,17 +248,37 @@ class RandomForestClassifier(object):
             """
 
         output = np.zeros((len(X), self.__num_categories))
-        self.__X = X.copy()
-        self.__proba = np.zeros((len(X), self.__num_categories))
+        #self.__X = X.copy()
+        #self.__proba = np.zeros((len(X), self.__num_categories))
 
         for tree_index in np.arange(self.__num_trees):
-            self.__loadTreeFromForest(tree_index)
-            self.__searchNodes(np.arange(len(X)))
-            output += self.__proba
+            #self.__loadTreeFromForest(tree_index)
+            #self.__searchNodes(np.arange(len(X)))
+            #output += self.__proba
+            proba = np.zeros((len(X), self.__num_categories))
+            children_left = self.__trees[tree_index]["children_left"]
+            children_right = self.__trees[tree_index]["children_right"]
+            features = self.__trees[tree_index]["feature"]
+            thresholds = self.__trees[tree_index]["threshold"]
+            tree_proba = self.__trees[tree_index]["proba"]
+            indexs = np.arange(X.shape[0], dtype=int)
+            """import numba
+            print("children_left", children_left.dtype, numba.typeof(children_left))
+            print("children_right", children_right.dtype, numba.typeof(children_right))
+            print("features", features.dtype, numba.typeof(features))
+            print("thresholds", thresholds.dtype, numba.typeof(thresholds))
+            print("tree_proba", tree_proba.dtype, numba.typeof(tree_proba))
+            print("indexs", indexs.dtype, numba.typeof(indexs))"""
+            if len(children_left) > sys.getrecursionlimit():
+                sys.setrecursionlimit(int(len(children_left)*1.2))
+            search_nodes(X, children_left, children_right, features, thresholds,
+                         tree_proba, proba, indexs, 0)
+            output += proba
+            #raise Exception("No need to continue")
 
         output /= self.__num_trees
 
-        del self.__X
+        #del self.__X
         return output
 
     @classmethod
@@ -235,8 +341,6 @@ class RandomForestClassifier(object):
         cls_instance.set_num_categories(num_categories)
         cls_instance.classes_ = classes
 
-        # TODO: remove old loading
-        """
         hdus = [hdul["{}{}".format(name_prefix, index)]
                 for index in range(num_trees)]
         trees = [{"children_left": hdu.data["children_left"].astype(np.int64),
@@ -246,11 +350,8 @@ class RandomForestClassifier(object):
                   "proba": hdu.data["proba"].astype(np.float64).reshape(
                     (hdu.data["proba"].shape[0], 1, hdu.data["proba"].shape[1])),
                 } for hdu in hdus]
-        """
-        cls_instance._RandomForestClassifier__trees = [hdul["{}{}".format(name_prefix, index)].data
-                              for index in range(num_trees)]
-        #cls_instance.set_trees(trees)
-
+        cls_instance.set_trees(trees)
+        
         return cls_instance
 
     def set_num_trees(self, num_trees):
@@ -293,9 +394,9 @@ class RandomForestClassifier(object):
                     array=self.__trees[index].get(field),
                     format=type,
                     )
-                for field, type in [("children_left", "I"),
-                                    ("children_right", "I"),
-                                    ("feature", "I"),
+                for field, type in [("children_left", "K"),
+                                    ("children_right", "K"),
+                                    ("feature", "K"),
                                     ("threshold", "D"),
                                     ("proba", "{}D".format(self.__num_categories))]
                 ]
